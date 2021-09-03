d676360096a7384b51d682154085c7e0f9c6b0ac
GhostCat <1169518718@qq.com>
2021-09-03 15:40:58 +0800

    修改着色器代码，变成更好看的渐变效果

--- src/shaders/box/fragment.fs
+++ src/shaders/box/fragment.fs
@@ -4,5 +4,6 @@ uniform float uTime;
 varying vec2 vUv;
 
 void main() {
-  gl_FragColor = vec4((sin(uTime) + 1.0) * vUv.x, (sin(uTime) + 1.0) * vUv.y, 0.8, 1);
+  vec3 col = 0.5 + 0.5*cos(uTime + vUv.xyx + vec3(0,2,4));
+  gl_FragColor = vec4(col, 1);
 }
